9065ac00e9adc3f10a03487c46b77bfba4fe1baa
AlexMantoux <alex.mantoux@gmail.com>
2026-02-09 15:12:20 +0100

    Add return value

--- ex6/ft_garden_summary.py
+++ ex6/ft_garden_summary.py
@@ -1,4 +1,4 @@
-def ft_garden_summary():
+def ft_garden_summary() -> None:
     garden_name: str = str(input("Enter garden name: "))
     plants_number: int = int(input("Enter number of plants: "))
     print("Garden:", garden_name)
